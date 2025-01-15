4dd8dc800a81ceefe605b1d11df5d70c602bb6f0
Andrew Owyang <36319661+Ajowyang@users.noreply.github.com>
2025-01-14 22:51:51 -0800

    Time: 19 ms (43.12%), Space: 19.3 MB (37.41%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -8,18 +8,13 @@ class Solution:
         #each space scores min(side_max) - height
         while left <= right:
             if high_left > high_right:
-                
                 result += max(0, (min(high_left, high_right) - height[right] ))
                 if height[right] > high_right:
                     high_right = height[right]
-                  
                 right -= 1
             else:
-                
                 result += max(0, (min(high_left, high_right) - height[left] ))
                 if height[left] > high_left:
                     high_left = height[left]
-                    
                 left += 1
-
         return result    
