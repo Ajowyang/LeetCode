a6676363bce9254f1d8208024cb25dbac82266bf
Andrew Owyang <36319661+Ajowyang@users.noreply.github.com>
2025-01-18 04:25:03 -0800

    Time: 86 ms (12.87%), Space: 17.8 MB (29.35%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,15 +7,12 @@ class Solution:
         while right < len(s):
             if s[right] not in letter_set:
                 letter_set.add(s[right])
-                print(letter_set)
                 max_len = max(len(letter_set), max_len)
                 right += 1
             else:
                 while s[right] in s[left:right]:
-                    print(f'while {s[right]} in {s[left:right+1]}')
                     left += 1
                 letter_set = set(list(s[left:right+1]))
-                print(letter_set)
                 right += 1
             
         return max_len
