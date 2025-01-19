6ef6ee831453e14c29f677aa2a8df85e7f8188c4
Andrew Owyang <36319661+Ajowyang@users.noreply.github.com>
2025-01-19 01:53:44 -0800

    Time: 0 ms (100%), Space: 18.6 MB (42.95%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,15 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if target not in set(nums):
-            return -1
-        if len(nums) == 1:
-            return 0
-        ndx = len(nums) // 2
-        if target == nums[ndx]:
-            return ndx
-        elif target < nums[ndx]:
-            return self.search(nums[0:ndx], target)
-        elif target > nums[ndx]:
-            return ndx+ 1 + self.search(nums[ndx+1: len(nums)], target)
-
-
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            midway = (right + left) // 2
+            if nums[midway] == target:
+                return midway
+            elif target < nums[midway]:
+                right = midway - 1
+            elif target > nums[midway]:
+                left = midway + 1
+        return -1
