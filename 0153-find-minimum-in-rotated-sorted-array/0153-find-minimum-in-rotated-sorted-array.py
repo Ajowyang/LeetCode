bf124f7d22188b93cb64505229d96dc7290ed376
Andrew Owyang <36319661+Ajowyang@users.noreply.github.com>
2025-01-19 17:10:43 -0800

    Time: 0 ms (100%), Space: 17.8 MB (56.87%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -5,14 +5,12 @@ class Solution:
 
         while left <= right:
             midway = (left + right) // 2
-            print(f'left: nums[{left}] = {nums[left]}, midway: nums[{midway}] = {nums[midway]} right: nums[{right}] = {nums[right]}')
+            # print(f'left: nums[{left}] = {nums[left]}, midway: nums[{midway}] = {nums[midway]} right: nums[{right}] = {nums[right]}')
             if (right - left) == 1:
                 return (nums[left] if nums[left] < nums[right] else nums[right])
             if left == right:
                 return nums[left]
-            print(nums)
             
-           
             if (nums[left] > nums[midway] and nums[right] > nums[midway]) :
                 right = midway 
             elif (nums[left] < nums[midway] and nums[right] < nums[midway]):
