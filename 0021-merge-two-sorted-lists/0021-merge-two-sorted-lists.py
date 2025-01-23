6eac1c6c45e9db467ac3dddf2837b28b78ff8adb
Andrew Owyang <36319661+Ajowyang@users.noreply.github.com>
2025-01-22 17:28:15 -0800

    Time: 0 ms (100%), Space: 17.7 MB (39.29%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -12,7 +12,7 @@ class Solution:
             if list1.val < list2.val:
                 tail.next = list1
                 list1 = list1.next
-                print(list1)
+                
             else:
                 tail.next = list2
                 list2 = list2.next
